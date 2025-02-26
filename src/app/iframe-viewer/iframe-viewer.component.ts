09b89529b051105396d4db50636b54db6086495d
Praveen Kumar <praveenjillu17@gmail.com>
2025-02-26 08:45:19 +0530

    Add files via upload

--- src/app/iframe-viewer/iframe-viewer.component.ts
+++ src/app/iframe-viewer/iframe-viewer.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-iframe-viewer',
-  templateUrl: './iframe-viewer.component.html',
-  styleUrls: ['./iframe-viewer.component.scss']
+	selector: 'app-iframe-viewer',
+	templateUrl: './iframe-viewer.component.html',
+	styleUrls: ['./iframe-viewer.component.scss']
 })
 export class IframeViewerComponent {
-  currentProjectUrl: SafeResourceUrl;
+	currentProjectUrl: SafeResourceUrl;
 
-  constructor(private sanitizer: DomSanitizer) {
-    this.currentProjectUrl = this.sanitizeUrl('http://localhost:4201'); // Default project
-  }
+	constructor(private sanitizer: DomSanitizer) {
+		this.currentProjectUrl = this.sanitizeUrl('http://localhost:4201'); // Default project
+	}
 
-  loadProject(url: string) {
-    this.currentProjectUrl = this.sanitizeUrl(url);
-  }
+	loadProject(url: string) {
+		this.currentProjectUrl = this.sanitizeUrl(url);
+	}
 
-  sanitizeUrl(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
+	sanitizeUrl(url: string): SafeResourceUrl {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+	}
 }
 
